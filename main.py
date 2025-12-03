0771fa041c04cffa013a3608a5fa5e6b3f4ce427
kakaomame <nattyannkunndesu@icloud.com>
2025-12-03 21:26:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2326,7 +2326,7 @@ RENDER_URL = os.environ.get("RENDER_URL", "https://rei-1.onrender.com")
 
 
 @app.route('/wasm3', methods=['GET'])
-  def wasm():
+def pokeque():
     """最初のURL入力フォームを表示"""
     return render_template('wasmv1.html')
 
