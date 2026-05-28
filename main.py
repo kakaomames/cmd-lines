a7ec30688321cae6dcb0fc256da5def05dd018a7
kakaomame <nattyannkunndesu@icloud.com>
2026-05-28 23:40:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ from flask import Flask, request, jsonify, render_template, Response
 
 
 # スマホ基地の最新URLが保管されている神のRawリンク
-RAW_URL_CONFIG = "https://raw.githubusercontent.com/kakaomames/yt-dlp-yt-dlp-s25-Xiaomi/refs/heads/main/urls.json"
+RAW_URL_CONFIG = "https://raw.githubusercontent.com/kakaomames/yt-dlp-yt-dlp-s25/refs/heads/main/urls.json"
 
 # Vercel環境で ffmpeg を動かすためのバイナリ調達関数
 def ensure_ffmpeg():
